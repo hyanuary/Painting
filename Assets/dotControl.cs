466163a53ead8bf5170a77ddf203a943926bfdad
SAE\hyanuary <1003495@student.sae.edu.au>
2017-07-18 15:01:53 +1000

    painting done

--- Assets/dotControl.cs
+++ Assets/dotControl.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class dotControl : MonoBehaviour {
 
+    paintGM paint;
+
 	// Use this for initialization
 	void Start () {
         GetComponent<Transform>().localScale = new Vector2(paintGM.currentScale, paintGM.currentScale);
-		
+        paint = GetComponent<paintGM>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+        if(Input.GetKeyDown(KeyCode.Delete))
+        {
+            Destroy(this.gameObject);
+        }
+
 	}
 
     private void OnMouseOver()
--- Assets/paintGM.cs
+++ Assets/paintGM.cs
@@ -11,6 +11,8 @@ public class paintGM : MonoBehaviour {
 
     public Texture2D text2d;
 
+    public GameObject texture1;
+
     Vector2 lastMousePos;
 
     //public bool onSomething;
@@ -19,16 +21,31 @@ public class paintGM : MonoBehaviour {
 	void Start () {
 
         lastMousePos = new Vector2(-1f, -1f);
-        text2d = new Texture2D(512, 512);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        text2d = new Texture2D(1024, 512);
+        texture1.GetComponent<Renderer>().materials[1].SetTexture("_MainTex", text2d);
+        for (int y = 0; y < 512; y++)
+            for (int x = 0; x < 1024; x++)
+                text2d.SetPixel(x, y, Color.white);
+        text2d.Apply();
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 
         Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
        
+        //deleting the whole scene
+        if(Input.GetKey(KeyCode.Delete))
+        {
+            for (int y = 0; y < 512; y++)
+                for (int x = 0; x < 1024; x++)
+                    text2d.SetPixel(x, y, Color.white);
+            text2d.Apply();
+        }
 
-        
+        //drawing part
             if (Input.GetKey(mouseLeft))
             {
             if (lastMousePos.x != -1f)
@@ -40,6 +57,8 @@ public class paintGM : MonoBehaviour {
                 {
                     Vector2 objPosition = Camera.main.ScreenToWorldPoint(lastMousePos + offset * i);
                     Instantiate(baseDot, objPosition, baseDot.rotation);
+                    DrawCircle((int)remap(-9.3f, 9.3f, 0, 1023, objPosition.x), (int)remap(-5, 5, 0, 511, objPosition.y), 5, Color.black);
+                    text2d.Apply();
 
                 }
 
@@ -48,6 +67,8 @@ public class paintGM : MonoBehaviour {
             {
                 Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
                 Instantiate(baseDot, objPosition, baseDot.rotation);
+                DrawCircle((int)remap(-9.3f, 9.3f, 0, 1023, objPosition.x), (int)remap(-5, 5, 0, 511, objPosition.y), 5, Color.black);
+                text2d.Apply();
             }
                 lastMousePos = mousePosition;
                
@@ -55,6 +76,7 @@ public class paintGM : MonoBehaviour {
             else
             {
                 lastMousePos = new Vector2(-1f, -1f);
+                text2d.Apply();
             }
         
         
@@ -65,5 +87,22 @@ public class paintGM : MonoBehaviour {
         
     }
 
-   
+    void DrawCircle (int cx, int cy, int rad, Color col)
+    {
+        for(int x = Mathf.Max(cx - rad, 0); x<= Mathf.Min(cx + rad, text2d.width - 1); x++ )
+        {
+            for(int y = Mathf.Max(cy - rad, 0); y <= Mathf.Min(cy + rad, text2d.height-1); y++ )
+            {
+                text2d.SetPixel(x, y, col);
+            }
+        }
+    }
+
+    // give a source and destination, start and end, 
+    float remap(float src_range_start, float src_range_end, float dst_range_start, float dst_range_end, float value_to_remap)
+	{
+		return ((dst_range_end - dst_range_start) * (value_to_remap - src_range_start)) / (src_range_end - src_range_start) + dst_range_start;
+	}
+  
+
 }
